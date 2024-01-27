fba9bdd21e6c301fa7de0e01dccd5af2e1013f1d
saklani <50768838+saklani@users.noreply.github.com>
2024-01-27 17:17:23 +0530

    basic token support

--- liteseed.lua
+++ liteseed.lua
@@ -1,5 +1,16 @@
+local json = require('json')
 local utils = require("utils")
-Balances = Balances or {}
+
+if not Balances then Balances = { [ao.id] = 100000000000000 } end
+
+if Name ~= 'My Coin' then Name = 'My Coin' end
+
+if Ticker ~= 'COIN' then Ticker = 'COIN' end
+
+if Denomination ~= 10 then Denomination = 10 end
+
+if not Logo then Logo = 'optional arweave TXID of logo image' end
+
 Stakers = Stakers or {}
 Reputations = Reputations or {}
 
@@ -88,3 +99,110 @@ function Slash(msg)
 	Stakers[staker].amount = Stakers[staker].amount - amount
 	Balances[sender] = (Balances[sender] or 0) + amount 
 end
+
+Handlers.add('info', Handlers.utils.hasMatchingTag('Action', 'Info'), function(msg)
+  ao.send(
+    { Target = msg.From, Tags = { Name = Name, Ticker = Ticker, Logo = Logo, Denomination = tostring(Denomination) } })
+end)
+
+--[[
+     Balance
+   ]]
+--
+Handlers.add('balance', Handlers.utils.hasMatchingTag('Action', 'Balance'), function(msg)
+  local bal = '0'
+
+  -- If not Target is provided, then return the Senders balance
+  if (msg.Tags.Target and Balances[msg.Tags.Target]) then
+    bal = tostring(Balances[msg.Tags.Target])
+  elseif Balances[msg.From] then
+    bal = tostring(Balances[msg.From])
+  end
+
+  ao.send({
+    Target = msg.From,
+    Tags = { Target = msg.From, Balance = bal, Ticker = Ticker, Data = json.encode(tonumber(bal)) }
+  })
+end)
+
+--[[
+     Balances
+   ]]
+--
+Handlers.add('balances', Handlers.utils.hasMatchingTag('Action', 'Balances'),
+  function(msg) ao.send({ Target = msg.From, Data = json.encode(Balances) }) end)
+
+--[[
+     Transfer
+   ]]
+--
+Handlers.add('transfer', Handlers.utils.hasMatchingTag('Action', 'Transfer'), function(msg)
+  assert(type(msg.Tags.Recipient) == 'string', 'Recipient is required!')
+  assert(type(msg.Tags.Quantity) == 'string', 'Quantity is required!')
+
+  if not Balances[msg.From] then Balances[msg.From] = 0 end
+
+  if not Balances[msg.Tags.Recipient] then Balances[msg.Tags.Recipient] = 0 end
+
+  local qty = tonumber(msg.Tags.Quantity)
+  assert(type(qty) == 'number', 'qty must be number')
+
+  if Balances[msg.From] >= qty then
+    Balances[msg.From] = Balances[msg.From] - qty
+    Balances[msg.Tags.Recipient] = Balances[msg.Tags.Recipient] + qty
+
+    --[[
+         Only send the notifications to the Sender and Recipient
+         if the Cast tag is not set on the Transfer message
+       ]]
+    --
+    if not msg.Tags.Cast then
+      -- Send Debit-Notice to the Sender
+      ao.send({
+        Target = msg.From,
+        Tags = { Action = 'Debit-Notice', Recipient = msg.Tags.Recipient, Quantity = tostring(qty) }
+      })
+      -- Send Credit-Notice to the Recipient
+      ao.send({
+        Target = msg.Tags.Recipient,
+        Tags = { Action = 'Credit-Notice', Sender = msg.From, Quantity = tostring(qty) }
+      })
+    end
+  else
+    ao.send({
+      Target = msg.Tags.From,
+      Tags = { Action = 'Transfer-Error', ['Message-Id'] = msg.Id, Error = 'Insufficient Balance!' }
+    })
+  end
+end)
+
+--[[
+    Mint
+   ]]
+--
+Handlers.add('mint', Handlers.utils.hasMatchingTag('Action', 'Mint'), function(msg, env)
+  assert(type(msg.Tags.Quantity) == 'string', 'Quantity is required!')
+
+  if msg.From == env.Process.Id then
+    -- Add tokens to the token pool, according to Quantity
+    local qty = tonumber(msg.Tags.Quantity)
+    Balances[env.Process.Id] = Balances[env.Process.Id] + qty
+  else
+    ao.send({
+      Target = msg.Tags.From,
+      Tags = {
+        Action = 'Mint-Error',
+        ['Message-Id'] = msg.Id,
+        Error = 'Only the Process Owner can mint new ' .. Ticker .. ' tokens!'
+      }
+    })
+  end
+end)
+
+
+
+Handlers.add('stake', Handlers.utils.hasMatchingTag('Action', 'Stake'), Stake(msg))
+Handlers.add('unstake', Handlers.utils.hasMatchingTag('Action', 'Unstake'), Unstake(msg))
+Handlers.add('punish', Handlers.utils.hasMatchingTag('Action', 'Punish'), Punish(msg, env))
+Handlers.add('reward', Handlers.utils.hasMatchingTag('Action', 'Reward'), Reward(msg, env))
+Handlers.add('slash', Handlers.utils.hasMatchingTag('Action', 'Slash'), Slash(msg))
